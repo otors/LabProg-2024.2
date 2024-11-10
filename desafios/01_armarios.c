5c5a51d33b27255a060fbecc38d78863376da5d1
Oto Serafim <186635411+otors@users.noreply.github.com>
2024-11-10 01:00:23 -0300

    Update .gitignore e otimizações em desafios

--- desafios/01_armarios.c
+++ desafios/01_armarios.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <unistd.h>
 
-void mostrar_tabela(unsigned char controle); // Protótipo da função mostra_tabela
+void mostrar_tabela(unsigned char controle, unsigned char tempo); // Protótipo da função mostra_tabela
 
 int main(int argc, char const *argv[])
 {
@@ -23,42 +23,31 @@ int main(int argc, char const *argv[])
 
         unsigned char masc = 1; // Inicialização da mascara antes de cada operação
         if (escolha == 1) {
-            // Mostrando ao usuário os armários antes de ocupar
-            printf("\e[1;1H\e[2J"); // Limpar console
-            puts("----===== Armários =====----");
-            mostrar_tabela(armarios), sleep(1);
+            mostrar_tabela(armarios, 1); // Mostrando ao usuário os armários antes de ocupar
 
             // Handler do caso em que todos os armarios estão ocupados
-            if (armarios == 255) {
+            if (armarios == ((1 << (8*sizeof(armarios)))-1)) {
                 puts("Todos os armários estão ocupados!"), sleep(1);
                 continue;
             }
             
-            // Avisando ao usuário que o programa está efetuando a operação
-            puts("Ocupando um armário..."), sleep(1);
+            puts("Ocupando um armário..."), sleep(1); // Avisando ao usuário que o programa está efetuando a operação
             
             // Ocupando um armario aleatório
             char random; // Variavel para guardar o armário gerado aleatoriamente
             do {
                 masc = 1; // Resetando a masc para cada iteração
-                random = rand() % 8;
+                random = rand() % 8*sizeof(armarios);
                 masc <<= random; // Sorteando um armario dentre os possiveis
             } while(armarios & masc);
             armarios |= masc;
 
-            // Mostrando ao usuário os armários após ocupar o armário aleatório 
-            printf("\e[1;1H\e[2J"); // Limpar console
-            puts("----===== Armários =====----");
-            mostrar_tabela(armarios), sleep(1);         
-
-            // Informa ao usuário o armário que foi ocupado
-            printf("O armário %d foi ocupado!\n", random), sleep(1);   
+            mostrar_tabela(armarios, 1); // Mostrando ao usuário os armários após ocupar o armário aleatório 
+            printf("O armário %d foi ocupado!\n", random), sleep(1); // Informa ao usuário o armário que foi ocupado
 
         } else if (escolha == 2) {
             // Mostrando ao usuário os armários antes de desocupar
-            printf("\e[1;1H\e[2J"); // Limpar console
-            puts("----===== Armários =====----");
-            mostrar_tabela(armarios), sleep(1);
+            mostrar_tabela(armarios, 1);
 
             // Handler do caso em que todos os armarios estão desocupados
             if (!armarios) {
@@ -69,21 +58,18 @@ int main(int argc, char const *argv[])
             // Pedindo ao usuário o armário que ele deseja liberar
             char liberar;
             do {
-                printf("Digite o armário que deseja liberar (0 a 7): ");
+                printf("Digite o armário que deseja liberar (0 a %ld): ", 8*sizeof(armarios)-1);
                 scanf("%hhd", &liberar);
-            } while (liberar < 0 || liberar > 7);
+            } while (liberar < 0 || liberar > (8*sizeof(armarios)-1));
 
-            // Avisando ao usuário que o programa está efetuando a operação
-            printf("Liberando armário %d...\n", liberar), sleep(1);
+            printf("Liberando armário %d...\n", liberar), sleep(1); // Avisando ao usuário que o programa está efetuando a operação
 
             // Liberando o armário selecionado
             masc <<= liberar;
             armarios &= ~masc;
 
             // Mostrando ao usuário os armários após desocupar o armário selecionado
-            printf("\e[1;1H\e[2J"); // Limpar console
-            puts("----===== Armários =====----");
-            mostrar_tabela(armarios), sleep(1);
+            mostrar_tabela(armarios, 1);
             printf("O armário %d foi liberado!\n", liberar), sleep(1);
 
         }
@@ -101,9 +87,14 @@ int main(int argc, char const *argv[])
 
     *controle: variavel de controle para observação dos bits. Troque 
                o tipo da variavel para mais bits.
+    *tempo: duração da exibição da tabela em segundos.
 */
-void mostrar_tabela(unsigned char controle) {
+void mostrar_tabela(unsigned char controle, unsigned char tempo) {
     char tam = 8*sizeof(controle)-1; // Variavel para gerenciar o tamanho da tabela
+
+    // Uso especifico deste programa, mude o remova de arcodo com o necessário
+    printf("\e[1;1H\e[2J"); // Limpar console
+    puts("----===== Armários =====----");
     
     printf("--");
     for (int i = tam; i >= 0; i--) 
@@ -126,4 +117,6 @@ void mostrar_tabela(unsigned char controle) {
     for (int i = tam; i >= 0; i--) 
         i > 9 ? printf("----") : printf("---"); // Tabela correta para tipos com mais de 1 byte
     printf("--\n");
+
+     sleep(tempo);
 }
